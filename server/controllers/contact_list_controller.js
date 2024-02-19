eae53ef8ca094f1c13bf7fc6b19252408f9743db
Satoshi Fujimura <avi1534x@gmail.com>
2024-02-19 20:50:30 +0900

    japanese

--- server/controllers/contact_list_controller.js
+++ server/controllers/contact_list_controller.js
@@ -9,7 +9,7 @@ function prepareConfirmationEmail(reqBody) {
 	const subject = "Please Confirm Your Email Address";
 	const url = formatUrl(Settings.url) + '/success';
 	const link = "<a href='" + url + "'>this link</a>"
-	const mailText = "Thanks for signing up! Click " + link + " to sign up!  This link will be active for 24 hours.";
+	const mailText = "Thanks for signing up! Click " + link + " to sign up!  This link will be active for 24 hours.\nメールマガジンへの登録申し込みありがとうございます。この" + link + "をクリックして登録を完了してください。";
 
 	var emailBody = {
 	  personalizations: [
